c5729b70dc8c9d9ff0364d632057addd8fa1fcca
FlavienBuron <60775271+FlavienBuron@users.noreply.github.com>
2020-03-26 11:13:48 +0100

    Cleaner code, bonus exo, led toggle

--- S02_wall_follow/S02_wall_follow.c
+++ S02_wall_follow/S02_wall_follow.c
@@ -20,16 +20,30 @@
 #define T_D 0.15
 #define PID_WALL_FOLLOW_TARGET 200
 #define PID_MAX_DS 200
+// define the states, FIND a wall & FOLLOW a wall
+#define LOVER 0
+#define FOLLOW 1
+// define the values for wall detection
+#define EMPTY 0
+#define FOUND 1
+//define values for turn direction
+#define LEFT 'L'
+#define RIGHT 'R'
+
+
 double a = 1, b = 5, c = 5, d = 0;
 
-int STATE = 0;
-int WALL = 0;
-char D;
+
+int STATE = LOVER;
+int WALL = EMPTY;
+int ANGLE = 0;
+char Direction;
 int counter = 0;
-int x = 0;
 int rand_int;
 
-void clockwise(), anticlockwise(), lover();
+// methods declaration
+void clockwise(), anticlockwise(), lover(), led_on(char side), led_off(char side);
+int random_int();
 
 double error = 0;
 double deriv = 0;
@@ -81,105 +95,114 @@ void robot_loop() {
         double ds_left = (NORM_SPEED * prox_left) / MAX_PROX;
         double speed_right = bounded_speed(NORM_SPEED - ds_right);
         double speed_left = bounded_speed(NORM_SPEED - ds_left);
-        printf("%f %f\n",speed_left, speed_right );
-        printf("STATE: %d, WALL: %d, Counter: %d\n", STATE, WALL, rand_int);
-
-        if(STATE == 0 && WALL == 0){
+        if(STATE == LOVER && WALL == EMPTY){
           if (speed_right < 0.1 && speed_left < 0.1) {
-            WALL = 1;
-            printf("%d, %d\n",prox_values[0], prox_values[7]);
-            srand(time(NULL));
-            rand_int = rand() % 2;
-            D = rand_int == 0 ? 'R' : 'L';
+            WALL = FOUND;
+            Direction = (random_int() == 0) ? RIGHT : LEFT;
           } else{
             lover(speed_left, speed_right);
           }
         }
-        else if(STATE == 0 && WALL == 1){
-          if (D == 'R') {
+        else if(STATE == LOVER && WALL == FOUND){
+          if (Direction == RIGHT) {
             if (counter <= 17) {
               clockwise();
               counter++;
             }else{
-              STATE = 1;
-              WALL = 0;
+              STATE = FOLLOW;
+              WALL = EMPTY;
               counter = 0;
             }
-            printf("STATE: %d, WALL: %d, D: %c, Counter: %d,\n", STATE, WALL, D, counter);
-          } else if (D == 'L') {
+          } else if (Direction == LEFT) {
             if (counter <= 17) {
               anticlockwise();
               counter++;
             }else{
-              STATE = 1;
-              WALL = 0;
+              STATE = FOLLOW;
+              WALL = EMPTY;
               counter = 0;
             }
-            printf("STATE: %d, WALL: %d,D: %c, Counter: %d, x: %d\n", STATE, WALL, D, counter, x);
           }
-
         }
-        else if(STATE == 1) {
-          if (WALL == 1) {
-            if (D == 'R') {
+        else if(STATE == FOLLOW) {
+          //If a wall has been found turn in the current direction
+          if (WALL == FOUND) {
+            if (Direction == RIGHT) {
               if (counter < 17) {
                 clockwise();
                 counter++;
               }else{
-                STATE = 1;
-                WALL = 0;
+                STATE = FOLLOW;
+                WALL = EMPTY;
                 counter = 0;
               }
-              printf("STATE: %d, WALL: %d,D: %c, Counter: %d, x: %d\n", STATE, WALL, D, counter, x);
-            } else if (D == 'L') {
+            } else if (Direction == LEFT) {
               if (counter < 17) {
                 anticlockwise();
                 counter++;
               }else{
-                STATE = 1;
-                WALL = 0;
+                STATE = FOLLOW;
+                WALL = EMPTY;
                 counter = 0;
               }
             }
-            printf("STATE: %d, WALL: %d,D: %c, Counter: %d, x: %d\n", STATE, WALL, D, counter, x);
-
           }
-          if (WALL == 0) {
+          //If there is no wall in front register when one is found
+          if (WALL == EMPTY) {
             if ((prox_values[0] > (PID_MAX_DS-20)) && (prox_values[7] > (PID_MAX_DS))) {
-              WALL = 1;
+              WALL = FOUND;
             }
-
-          double a = 1, b = 5, c = 5, d = 0;
+/////////////////////////////// Follow the wall and register logs /////////////////////////////////////////////////////////////////////////////
+          double a = 1, b = 5, c = 5, d = 2;
           double prox_right = (a * prox_values[0] + b * prox_values[1] + c * prox_values[2] + d * prox_values[3]) / (a+b+c+d);
           double prox_left = (a * prox_values[7] + b * prox_values[6] + c * prox_values[5] + d * prox_values[4]) / (a+b+c+d);
+          if (prox_right < PID_WALL_FOLLOW_TARGET/3 || prox_left < PID_WALL_FOLLOW_TARGET/3) {
+            ANGLE = 1;
+          }
           //compute PID response according to IR sensor value
-          if (D == 'R'){
+          if (Direction == RIGHT){
+            if (ANGLE == 1) {
+              if (counter <= 30) {
+                set_speed(NORM_SPEED, 0.2*NORM_SPEED);
+                counter++;
+              }else{
+                ANGLE = 0;
+                counter = 0;
+              }
+            }
+            // adjust speed values to turn away from obstacle, with wall to the left
             double ds = pid(prox_left);
             double speed_right = (abs(ds)>PID_MAX_DS ? 0 : NORM_SPEED) - ds;
             double speed_left  = (abs(ds)>PID_MAX_DS ? 0 : NORM_SPEED) + ds;
             fprintf(log, "%d,%f,%f,%f,%f,%f,%f\n", loop_counter, K*error, K*integ/T_I, K*deriv*T_D, ds, speed_left,speed_right);
+            led_off(RIGHT);
+            led_on(LEFT);
+            set_speed(bounded_speed(speed_left), bounded_speed(speed_right));
 
-          set_speed(bounded_speed(speed_left), bounded_speed(speed_right));
-          } else if (D == 'L'){
-          //double ds = (D == 'L') ? pid(prox_right) : -pid(prox_left);
-          //printf("R: %f, L: %f, ds: %f\n", pid(prox_right), pid(prox_left), ds);
-          // adjust speed values to turn away from obstacle
-          double ds = pid(prox_right);
-          double speed_right = (abs(ds)>PID_MAX_DS ? 0 : NORM_SPEED) + ds;
-          double speed_left  = (abs(ds)>PID_MAX_DS ? 0 : NORM_SPEED) - ds;
-          fprintf(log, "%d,%f,%f,%f,%f,%f,%f\n", loop_counter, K*error, K*integ/T_I, K*deriv*T_D, ds, speed_left,speed_right);
-
-          set_speed(bounded_speed(speed_left), bounded_speed(speed_right));
-          }
-          // write a line of data in log file
-          //fprintf(log, "%d,%f,%f,%f,%f,%f,%f\n", loop_counter, K*error, K*integ/T_I, K*deriv*T_D, ds, speed_left,speed_right);
-
-          //set_speed(bounded_speed(speed_left), bounded_speed(speed_right));
+          } else if (Direction == LEFT){
+            if (ANGLE == 1) {
+              if (counter <= 30) {
+                set_speed(0.2*NORM_SPEED, NORM_SPEED);
+                counter++;
+              }else{
+                ANGLE = 0;
+                counter = 0;
+              }
+            }
+            // adjust speed values to turn away from obstacle, with wall to the right
+            double ds = pid(prox_right);
+            double speed_right = (abs(ds)>PID_MAX_DS ? 0 : NORM_SPEED) + ds;
+            double speed_left  = (abs(ds)>PID_MAX_DS ? 0 : NORM_SPEED) - ds;
+            fprintf(log, "%d,%f,%f,%f,%f,%f,%f\n", loop_counter, K*error, K*integ/T_I, K*deriv*T_D, ds, speed_left,speed_right);
+            led_off(LEFT);
+            led_on(RIGHT);
+            set_speed(bounded_speed(speed_left), bounded_speed(speed_right));
 
           }
         }
       }
     }
+  }
 
 int main (int argc, char **argv) {
     #if SIMULATION
@@ -202,4 +225,38 @@ void clockwise(){
 
 void anticlockwise(){
     set_speed(-NORM_SPEED, NORM_SPEED);
+}
+
+int random_int(){
+  srand(time(NULL));
+  rand_int = rand() % 2;
+  return rand_int;
+}
+
+void led_on(char side){
+  switch (side) {
+    case RIGHT:
+    enable_led(0);
+    enable_led(1);
+    break;
+
+    case LEFT:
+    enable_led(3);
+    enable_led(2);
+    break;
+  }
+}
+
+void led_off(char side){
+  switch (side) {
+    case RIGHT:
+    disable_led(0);
+    disable_led(1);
+    break;
+
+    case LEFT:
+    disable_led(3);
+    disable_led(2);
+    break;
+  }
 }
