95c87a005bcbd0252782056f719e5c687fc0f7ef
FlavienBuron <60775271+FlavienBuron@users.noreply.github.com>
2020-04-08 12:32:21 +0200

    new color explorer (better) and alternate search

--- S03_alternate_search/S03_alternate_search.c
+++ S03_alternate_search/S03_alternate_search.c
@@ -2,80 +2,131 @@
 // Created by daniel on 02.04.20.
 //
 
+
 #define SIMULATION 1
 
 #if SIMULATION
-
-#include <stdbool.h>
-#include <unistd.h>
 #include "../API/webots/webotsAPI.h"
-
 #else
 #include "../API/epuck/epuckAPI.h"
 #endif
 
-void explorer(), lover(), fsm(), sendUpdateStates(),sendCurrentStatus();
-int receiveOtherStatus();
-void receiveUpdateState();
+void explorer(), lover(), stop();
 
-#define STEXPLORER 0
-#define STLOVER 1
-#define STEQUIL 2
+//typedef int bool;
+#define STOP 0
+#define LOVER 1
+#define EXPLORER 2
+#define MSG_LENGTH 5
+#define NB_ROBOT 2
 
+// control board
 
-#define NBRROBOT 2
+int init = 1;
+int EQ = 0;
+int ack = 0;
+int go = 0;
 
-int STATE = STEXPLORER;
+int STATE = LOVER;
 int SAW_OBSTACLE = 0;
-int equilibirum = 0;
-
+int HAS_STOPPED = 0;
 int a = 1;
-
 int b = 2;
 int c = 3;
 int d = 4;
 double e = 10.;
+
+char rcv[MSG_LENGTH];
+char tmp[MSG_LENGTH];
+char tmp2[MSG_LENGTH];
 
 
 void robot_setup() {
     init_robot();
     init_sensors();
     calibrate_prox();
+    init_communication();
 }
 
-int states[NBRROBOT][2];
-
-
 void robot_loop() {
-
-
+    int id = get_robot_ID();
     short int prox_values[8];
 
+
     while (robot_go_on()) {
-        int id = get_robot_ID();
 
-        printf("robot nr %d , State=%d , Obstacle=%d \n", id, states[id-1][0],states[id-1][1]);
 
+      receive_msg(rcv);
+      if (strcmp(rcv,"stop")==0){
+        ack++;
+        printf("The messages recieved id stop \n");
+
+      } else if (strcmp(rcv,"go")==0){
+        go++;
+        printf("The messages recieved id go \n");
+
+      }else if (strcmp(rcv,"EQ")==0){
+        EQ = 1;
+        printf("The messages recieved id EQ \n");
+
+      }
+
+
+        //printf("ID %d, %d, %d\n", id, SAW_OBSTACLE, STATE);
         get_prox_calibrated(prox_values);
         double prox_right = (a * prox_values[0] + b * prox_values[1] + c * prox_values[2] + d * prox_values[3]) / e;
         double prox_left = (a * prox_values[7] + b * prox_values[6] + c * prox_values[5] + d * prox_values[4]) / e;
-        double ds_right = (MAX_SPEED * prox_right) / MAX_PROX;
-        double ds_left = (MAX_SPEED * prox_left) / MAX_PROX;
-        double speed_right = bounded_speed(MAX_SPEED - ds_right);
-        double speed_left = bounded_speed(MAX_SPEED - ds_left);
+        double ds_right = (NORM_SPEED * prox_right) / MAX_PROX;
+        double ds_left = (NORM_SPEED * prox_left) / MAX_PROX;
+        double speed_right = bounded_speed(NORM_SPEED - ds_right);
+        double speed_left = bounded_speed(NORM_SPEED - ds_left);
 
-        int oldState = states[id -1][0];
-        receiveUpdateState(id);
-        int newState = states[id -1][0];
-
-        if(oldState==2 && newState ==0){
-            for (int i = 2000; i > 0; i--) {
+        if (STATE == EXPLORER) {
+            if (speed_right < NORM_SPEED*0.25 || speed_left < NORM_SPEED*0.25) {
+                SAW_OBSTACLE = 1;
+                explorer(speed_left, speed_right);
+            } else if ((speed_right > NORM_SPEED*0.5 && speed_left > NORM_SPEED*0.5) && SAW_OBSTACLE) {
+                SAW_OBSTACLE = 0;
+                lover(speed_left, speed_right);
+            } else {
                 explorer(speed_left, speed_right);
             }
-        }
-        fsm(id,speed_left,speed_right);
 
-    }
+        } else if (STATE == LOVER) {
+            if ((prox_right > 80.0 || prox_left > 80.0) && init == 1) {
+              if (ack == NB_ROBOT - 1) {
+                stop();
+                sprintf(tmp2, "%s", "go");
+                send_msg(tmp2);
+              }else {
+              sprintf(tmp, "%s", "stop");
+              send_msg(tmp);
+              stop();
+              init = 0;
+              }
+            } else if (speed_right < 0.0 && speed_left < 0.0) {
+              stop();
+              sprintf(tmp2, "%s", "EQ");
+              send_msg(tmp2);
+            } else {
+                lover(speed_left, speed_right);
+            }
+        } else if(STATE == STOP){
+            if(go == NB_ROBOT - 1){
+              lover(speed_left, speed_right);
+              go = 0;
+            }
+            if(EQ == 1){
+            EQ = 0;
+              if(init == 0){
+                explorer(speed_left, speed_right);
+              } else {
+                lover(speed_left, speed_right);
+                init = 0;
+              }
+            }
+        }
+     }
     cleanup_robot();
 }
 
@@ -89,121 +140,41 @@ int main(int argc, char **argv) {
     robot_loop();
 }
 
-void fsm(int robotNumber, double speed_left, double speed_right) {
-    int currentState = states[robotNumber - 1][0];
-    int currentSaw = states[robotNumber - 1][1];
-
-    switch (currentState){
-
-        case STEXPLORER:
-            if (speed_right < 0.75 || speed_left < 0.75) {
-                //the robot saw an obstacle
-                states[robotNumber - 1][1] = 1;
-                //explorer(robotNumber,speed_left, speed_right);
-            } else if ((speed_right > 3.2 && speed_left > 3.2) && currentSaw) {
-                //the robot is far form the obstacle
-                states[robotNumber - 1][1] = 0;
-                lover(robotNumber,speed_left, speed_right);
-            } else {
-                explorer(robotNumber,speed_left, speed_right);
-            }
-            break;
-
-        case STLOVER:
-            if (speed_right < 0.0 && speed_left < 0.0) {
-                states[robotNumber - 1][0] = STEQUIL;
-                sendUpdateStates(robotNumber);
-            } else {
-                lover(robotNumber,speed_left, speed_right);
-            }
-            break;
-
-        case STEQUIL:
-            set_speed(0,0);
-            sendCurrentStatus(robotNumber);
-            break;
-    }
-
-
-}
-
-void explorer(int robotNumber, double speed_left, double speed_right) {
-    states[robotNumber - 1][0] = STEXPLORER;
+void explorer(double speed_left, double speed_right) {
+    STATE = EXPLORER;
     a = 1;
     b = 2;
     c = 2;
     d = 1;
     e = a + b + c + d;
-    disable_led(0);
-    disable_led(1);
-    disable_led(2);
-    disable_led(3);
+    for(int i = 0; i < 4; i++){
+      disable_rgbled(i);
+      enable_rgbled(i, 0x0000ff);
+    }
+
     set_speed(speed_right, speed_left);
 }
 
-void lover(int robotNumber, double speed_left, double speed_right) {
-    states[robotNumber - 1][0] = STLOVER;
+void lover(double speed_left, double speed_right) {
+    STATE = LOVER;
     a = 5;
     b = 3;
     c = 2;
     d = 1;
     e = a + b + c + d;
-    enable_led(0);
-    enable_led(1);
-    enable_led(2);
-    enable_led(3);
+    for(int i = 0; i < 4; i++){
+      disable_rgbled(i);
+      enable_rgbled(i, 0xff0000);
+    }
     set_speed(speed_left, speed_right);
 
 }
 
-void sendUpdateStates(int robotID){
-    char tmp[5];
-    int newVal;
-    init_communication();
-
-    int otherStatus = receiveOtherStatus();
-    if (otherStatus == 2){newVal=STEXPLORER;}else{newVal=STLOVER;}
-
-    sprintf(tmp,"%d",newVal);
-    printf("robot %d sends an update %s \n \n", robotID, tmp);
-    send_msg(tmp);
-
-
+void stop(){
+  STATE = STOP;
+  for(int i = 0; i < 4; i++){
+    disable_rgbled(i);
+    enable_rgbled(i, 0x00ff00);
+  }
+  set_speed(0, 0);
 }
-
-void sendCurrentStatus(int robotID){
-    char tmp[5];
-    init_communication();
-    sprintf(tmp,"%d", states[robotID-1][0]);
-    printf("robot %d sends his current status %s \n", robotID, tmp);
-    send_msg(tmp);
-
-}
-
-void receiveUpdateState(int robotId){
-    char rcv[5];
-    int currentState = states[robotId - 1][0];
-    init_communication();
-    receive_msg(rcv);
-    if (strcmp(rcv,"ZZZZ")!=0 && currentState != atoi(rcv) && atoi(rcv)!=2) {
-        printf("robot %d receives a state update %s \n", robotId, rcv);
-        states[robotId-1][0] =  atoi(rcv);
-    }
-
-}
-
-int receiveOtherStatus(){
-    char rcv[5];
-    init_communication();
-    receive_msg(rcv);
-    printf("Otherrecieved=%s , test = %d",rcv, strcmp(rcv,"ZZZZ"));
-    if (strcmp(rcv,"ZZZZ")!=0) {
-        printf("robot receives a status %s \n", rcv);
-        return atoi(rcv);
-    }else{
-        return -1;
-    }
-
-}
-
-
