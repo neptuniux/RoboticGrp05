9f1b2a7d37c31b95b4c4394b85a71e0cd4faf1be
FlavienBuron <60775271+FlavienBuron@users.noreply.github.com>
2020-04-09 18:18:37 +0200

    Changed alternate and simultaneous search

--- S03_alternate_search/S03_alternate_search.c
+++ S03_alternate_search/S03_alternate_search.c
@@ -12,15 +12,16 @@
 #endif
 
 void explorer(), lover(), stop();
+int send_id(int), send(char[]);
 
 //typedef int bool;
 #define STOP 0
 #define LOVER 1
 #define EXPLORER 2
-//#define MSG_LENGTH 5
-#define NB_ROBOT 4    //MUST match number of robots on the field. Robots MUST be named starting from 1
+#define MSG_LENGTH 5
+#define NB_ROBOT 3    //MUST match number of robots on the field.
 
-// control board, i.e varaibles that control the transitions
+// control board, i.e variables that control the transitions
 int robot[NB_ROBOT];
 int robot_count=0;
 int init = 1;
@@ -40,15 +41,12 @@ int d = 4;
 double e = 10.;
 
 char rcv[MSG_LENGTH];
-char rcv2[MSG_LENGTH];
 char tmp[MSG_LENGTH];
-char tmp2[MSG_LENGTH];
 
 
 void robot_setup() {
     init_robot();
     init_sensors();
-    calibrate_prox();
     init_communication();
     // initialize robot[] to 0
     for (int r = 0; r < NB_ROBOT; r++) {
@@ -57,103 +55,98 @@ void robot_setup() {
 }
 
 void robot_loop() {
-    int id = get_robot_ID();
-    short int prox_values[8];
-    int init = 1;
-    int EQ = 0;
-    int ack = 0;
-    int go = 0;
-    int sent = 0;
+  int id = get_robot_ID();
+  short int prox_values[8];
+  int init = 1;
+  int EQ = 0;
+  int ack = 0;
+  int go = 0;
+  int sent = 0;
 
-    while (robot_go_on()) {
-      //controlles that robot_count doesn't go over the array size
-      if(robot_count >= NB_ROBOT){
-        robot_count = 0;
+  while (robot_go_on()) {
+    //control that robot_count doesn't go over the array size
+    if(robot_count >= NB_ROBOT){
+      robot_count = 0;
+    }
+    receive_msg(rcv);
+
+    int rcv_id = rcv[0]- 48;
+    printf("sent = %d\n", init);
+    if (rcv_id <= NB_ROBOT){
+      robot[robot_count] = rcv_id;
+      robot_count++;
+    } else if (strcmp(rcv,"go")==0){
+      go=1;
+      init = 0;
+    }else if (strcmp(rcv,"EQ")==0){
+      EQ = 1;
+      init = 0;
+      robot_count++;
+    }else if (sent == 1) {
+      sent = 0;
+      robot[robot_count] = id;
+      robot_count++;
+    }
+
+    get_prox_calibrated(prox_values);
+    double prox_right = (a * prox_values[0] + b * prox_values[1] + c * prox_values[2] + d * prox_values[3]) / e;
+    double prox_left = (a * prox_values[7] + b * prox_values[6] + c * prox_values[5] + d * prox_values[4]) / e;
+    double ds_right = (NORM_SPEED * prox_right) / MAX_PROX;
+    double ds_left = (NORM_SPEED * prox_left) / MAX_PROX;
+    double speed_right = bounded_speed(NORM_SPEED - ds_right);
+    double speed_left = bounded_speed(NORM_SPEED - ds_left);
+
+    if (STATE == EXPLORER) {
+      if (speed_right < NORM_SPEED*0.25 || speed_left < NORM_SPEED*0.25) {
+        SAW_OBSTACLE = 1;
+        explorer(speed_left, speed_right);
+      } else if ((speed_right > NORM_SPEED*0.5 && speed_left > NORM_SPEED*0.5) && SAW_OBSTACLE) {
+        SAW_OBSTACLE = 0;
+        lover(speed_left, speed_right);
+      } else {
+        explorer(speed_left, speed_right);
       }
-      receive_msg(rcv);
-      int rcv_id = rcv[0]- 48;
-      if (rcv_id <= NB_ROBOT){
-        robot[robot_count] = rcv_id;
-        robot_count++;
-      } else if (strcmp(rcv,"go")==0){
-        go=1;
-        init = 0;
-      }else if (strcmp(rcv,"EQ")==0){
-        EQ = 1;
-        robot_count++;
-      }else if (sent == 1) {
-        sent = 0;
-        robot[robot_count] = id;
-        robot_count++;
-      }
-
-        get_prox_calibrated(prox_values);
-        double prox_right = (a * prox_values[0] + b * prox_values[1] + c * prox_values[2] + d * prox_values[3]) / e;
-        double prox_left = (a * prox_values[7] + b * prox_values[6] + c * prox_values[5] + d * prox_values[4]) / e;
-        double ds_right = (NORM_SPEED * prox_right) / MAX_PROX;
-        double ds_left = (NORM_SPEED * prox_left) / MAX_PROX;
-        double speed_right = bounded_speed(NORM_SPEED - ds_right);
-        double speed_left = bounded_speed(NORM_SPEED - ds_left);
-
-        if (STATE == EXPLORER) {
-            if (speed_right < NORM_SPEED*0.25 || speed_left < NORM_SPEED*0.25) {
-                SAW_OBSTACLE = 1;
-                explorer(speed_left, speed_right);
-            } else if ((speed_right > NORM_SPEED*0.5 && speed_left > NORM_SPEED*0.5) && SAW_OBSTACLE) {
-                SAW_OBSTACLE = 0;
-                lover(speed_left, speed_right);
-            } else {
-                explorer(speed_left, speed_right);
-            }
-
-        } else if (STATE == LOVER) {
-            if ((prox_right > 80.0 || prox_left > 80.0) && init == 1) {
-              for(int i = 0; i< NB_ROBOT; i++){
-                if (robot[i] > 0){
-                  ack++;
-                }
-              }
-              if (ack == NB_ROBOT - 1) {
-                stop();
-                sprintf(tmp, "%d", id);
-                sprintf(tmp2, "%s", "go");
-                send_msg(tmp);
-                send_msg(tmp2);
-                sent = 1;
-                init = 0;
-              }else {
-              sprintf(tmp, "%d", id);
-              send_msg(tmp);
-              sent = 1;
-              init = 0;
-              stop();
-              }
-            } else if (speed_right < 0.0 && speed_left < 0.0) {
-              sprintf(tmp2, "%s", "EQ");
-              send_msg(tmp2);
-              EQ = 0;
-              stop();
-              robot_count++;
-            } else {
-                lover(speed_left, speed_right);
-            }
-        } else if(STATE == STOP){
-          if(go == 1 && robot[0] == id){
-            lover(speed_left, speed_right);
-            go = 0;
-          }
-          else if(EQ == 1){
-            if (robot_count < (NB_ROBOT - 1)) {
-              if (robot[robot_count + 1] == id) {
-                explorer(speed_left, speed_right);
-              }
-            } else if(robot_count == (NB_ROBOT - 1) && robot[0] == id){
-              explorer(speed_left, speed_right);
+    } else if (STATE == LOVER) {
+      if (speed_right < 0.0 && speed_left < 0.0) {
+        if (init == 1){
+          for(int i = 0; i< NB_ROBOT; i++){
+            if (robot[i] > 0){
+              ack++;
             }
           }
+          if (ack == NB_ROBOT - 1) {
+            stop();
+            sent = send_id(id);
+            init = send("go");
+          }else {
+            stop();
+            sent = send_id(id);
+          }
+        } else if (init == 0){
+          EQ = send("EQ");
+          stop();
+          robot_count++;
+        }
+      } else {
+        lover(speed_left, speed_right);
+      }
+    } else if(STATE == STOP){
+      if(go == 1 && robot[0] == id){
+        explorer(speed_left, speed_right);
+        go = 0;
+      }
+      else if(EQ == 1){
+        if (robot_count < (NB_ROBOT - 1)) {
+          if (robot[robot_count + 1] == id) {
+            explorer(speed_left, speed_right);
+          }
+        } else if(robot_count == (NB_ROBOT - 1) && robot[0] == id){
+          explorer(speed_left, speed_right);
         }
       }
-    cleanup_robot();
+    }
+  }
+  cleanup_robot();
 }
 
 int main(int argc, char **argv) {
@@ -203,4 +196,17 @@ void stop(){
     enable_rgbled(i, 0x00ff00);
   }
   set_speed(0, 0);
+}
+
+int send_id(int id){
+  sprintf(tmp, "%d", id);
+  send_msg(tmp);
+  return 1;
+}
+
+int send(char msg[]){
+  sprintf(tmp, "%s", msg);
+  send_msg(tmp);
+
+  return 0;
 }
--- S03_simultaneous_search/S03_simultaneous_search.c
+++ S03_simultaneous_search/S03_simultaneous_search.c
@@ -8,13 +8,14 @@
 #endif
 
 void explorer(), lover(), stop();
+int send_id(int), send(char[]);
 
 //typedef int bool;
 #define STOP 0
 #define LOVER 1
 #define EXPLORER 2
-//#define MSG_LENGTH 5
-#define NB_ROBOT 3    //MUST match number of robots on the field. Robots MUST be named starting from 1
+#define MSG_LENGTH 5
+#define NB_ROBOT 3    //MUST match number of robots on the field.
 
 // control board, i.e variables that control the transitions
 int robot[NB_ROBOT];
@@ -34,9 +35,7 @@ int d = 4;
 double e = 10.;
 
 char rcv[MSG_LENGTH];
-char rcv2[MSG_LENGTH];
 char tmp[MSG_LENGTH];
-char tmp2[MSG_LENGTH];
 
 
 void robot_setup() {
@@ -55,19 +54,16 @@ void robot_loop() {
     short int prox_values[8];
 
     while (robot_go_on()) {
-      //controlles that robot_count doesn't go over the array size
+      //control that robot_count doesn't go over the array size
       if(robot_count >= NB_ROBOT){
         robot_count = 0;
       }
-      //printf("ID = %d, go = %d\n",id, go);
       receive_msg(rcv);
       int rcv_id = rcv[0]- 48;
       if (rcv_id <= NB_ROBOT){
         robot[robot_count] = rcv_id;
         robot_count++;
-
       } else if (strcmp(rcv,"go")==0){
-      //printf("go\n");
         go=1;
       }else if (sent == 1) {
         sent = 0;
@@ -84,46 +80,34 @@ void robot_loop() {
         double speed_left = bounded_speed(NORM_SPEED - ds_left);
 
         if (STATE == EXPLORER) {
-            if (speed_right < NORM_SPEED*0.25 || speed_left < NORM_SPEED*0.25) {
-                SAW_OBSTACLE = 1;
-                explorer(speed_left, speed_right);
-            } else if ((speed_right > NORM_SPEED*0.5 && speed_left > NORM_SPEED*0.5) && SAW_OBSTACLE) {
-                SAW_OBSTACLE = 0;
-                lover(speed_left, speed_right);
-            } else {
-                explorer(speed_left, speed_right);
-            }
-
-        } else if (STATE == LOVER) {
-            if (speed_right < 0.0 && speed_left < 0.0) {
-                for(int i = 0; i< NB_ROBOT; i++){
-                  if (robot[i] > 0){
-                    ack++;
-                  }
-                }
-                if (ack == NB_ROBOT - 1) {
-                  printf("plop\n");
-                  sprintf(tmp, "%d", id);
-                  sprintf(tmp2, "%s", "go");
-                  send_msg(tmp);
-                  send_msg(tmp2);
-                  sent = 1;
-                  go = 1;
-
-                  stop();
-              }else {
-                sprintf(tmp, "%d", id);
-                send_msg(tmp);
-                sent = 1;
-                stop();
-              }
-            } else {
-                lover(speed_left, speed_right);
-            }
-        } else if(STATE == STOP){
-        for (int i = 0; i < 1; i++){
-          //printf("ID %d, go %d\n", id , go);
+          if (speed_right < NORM_SPEED*0.25 || speed_left < NORM_SPEED*0.25) {
+            SAW_OBSTACLE = 1;
+            explorer(speed_left, speed_right);
+          } else if ((speed_right > NORM_SPEED*0.5 && speed_left > NORM_SPEED*0.5) && SAW_OBSTACLE) {
+            SAW_OBSTACLE = 0;
+            lover(speed_left, speed_right);
+          } else {
+            explorer(speed_left, speed_right);
           }
+        } else if (STATE == LOVER) {
+          if (speed_right < 0.0 && speed_left < 0.0) {
+            for(int i = 0; i< NB_ROBOT; i++){
+              if (robot[i] > 0){
+                ack++;
+              }
+            }
+            if (ack == NB_ROBOT - 1) {
+              sent = send_id(id);
+              go = send("go");
+              stop();
+            }else {
+              sent = send_id(id);
+              stop();
+            }
+          } else {
+            lover(speed_left, speed_right);
+          }
+        } else if(STATE == STOP){
           if(go == 1){
             robot_count = 0;
             ack = 0;
@@ -132,7 +116,6 @@ void robot_loop() {
               robot[r] = 0;
             }
             explorer(speed_left, speed_right);
-            //printf("ID = %d\n", id);
           }
         }
       }
@@ -186,4 +169,16 @@ void stop(){
     enable_rgbled(i, 0x00ff00);
   }
   set_speed(0, 0);
+}
+
+int send_id(int id){
+  sprintf(tmp, "%d", id);
+  send_msg(tmp);
+  return 1;
+}
+
+int send(char msg[]){
+  sprintf(tmp, "%s", msg);
+  send_msg(tmp);
+  return 1;
 }
