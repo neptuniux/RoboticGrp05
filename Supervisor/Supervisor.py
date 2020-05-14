4e95bc545dd22df155afdfd296d564177a5650a0
BORCARD Daniel <daniel.borcard@unifr.ch>
2020-05-14 22:31:07 +0200

    robots now move toward the center of mass the weight are now adjustable for the center of mass and the rotation

---
+++ Supervisor/Supervisor.py
@@ -0,0 +1,160 @@
+"""Boids Supervisor."""
+
+from controller import *
+import math
+import struct
+
+PI = math.pi
+
+NB_ROBOTS = 9
+FOV = 0.2
+ANGLE = 2.356
+robot = Supervisor()
+
+timestep = int(robot.getBasicTimeStep())
+
+coord = (0, 0)
+prev = ()
+#m = rows
+m = NB_ROBOTS
+#n = col
+n = 2
+inBoid = [0 for i in range(NB_ROBOTS)];
+
+robot_node = [[0]*n for i in range(NB_ROBOTS)]
+orientationMatrix = [[0]*NB_ROBOTS for i in range(NB_ROBOTS)]
+distMatrix = [[0]*NB_ROBOTS for i in range(NB_ROBOTS)]
+trans_field = []
+rota_field = []
+emitter = robot.getEmitter("emitter")
+
+for i in range(NB_ROBOTS):
+    node = robot.getFromDef("EP"+str(i))
+    tf = node.getField("translation")
+    rf = node.getField("rotation")
+    trans_field.append(tf)
+    rota_field.append(rf)
+
+def getCoordinates(trans_field):
+    values = trans_field.getSFVec3f()
+    coord = (values[0], values[2])
+    return coord
+
+def getRotation(rota_field):
+    values = rota_field.getSFRotation()
+    rota = values[3]
+    return rota
+
+# Don't use transform() before using this function
+def avgRotation(*rotaList):
+    rotation = 0
+    total = 0
+    for i in rotaList:
+        for r in i:
+            if (r != 999):
+                rotation += r
+                total += 1
+    #return round(rotation / total, 5)
+    if total == 0 :
+        total = 1
+    #return (rotation / total)
+    #print(rotation / total)
+    #return f'{(rotation / total):.5f}'
+    #print(rotation / total)
+    return rotation / total
+
+def centerMass(*distList):
+    x = 0
+    y = 0
+    total = 0
+    for i in distList:
+        for d in i:
+            if (d != 999):
+                x += d[0]
+                y += d[1]
+                total += 1
+    if total == 0 :
+        total = 1
+    #center = (round(x/total, 5), round(y/total, 5))
+    center = ((x/total), (y/total))
+    return center
+
+
+def getDist(x1, y1, x2, y2):
+    dist = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+    return dist
+
+'''
+def isBehind(x1, y1, x2, y2):
+    x = x2-x1
+    y = y2-y1
+    Ax = x1 + FOV*math.cos(ANGLE)
+    Ay = y1 + FOV*math.sin(ANGLE)
+    Bx = x1 + FOV*math.cos(-ANGLE)
+    By = y1 + FOV*math.sin(-ANGLE)
+    angle = math.atan(x/y)
+
+    print("A = {},{}; B = {},{}; t = {},{}".format(Ax,Ay, Bx, By, x2-x1, y2-y1))
+'''
+
+#transform [0;pi][0;-pi] range to [0,2pi]
+def transform(rotation):
+    if rotation < 0:
+        rotation = 2*math.pi + rotation
+        #print(rotation)
+    return rotation
+
+def centerMassdiff(center_x,center_y,robot_x, robot_y):
+    delta = (math.atan2((center_y-robot_y),(center_x-robot_x)))
+    return f'{delta:.5f}'
+
+
+
+def rotationDiff(current, target):
+    if target == 0:
+        return f'{target:.5f}'
+    delta = target - current
+    #print("before = " + str(delta))
+    if abs(delta) > PI:
+        if current > target:
+            delta = (2*PI + target) - current
+        else:
+            delta = target - (2*PI + current)
+    return f'{delta:.5f}'
+
+counter = 1
+
+while (robot.step(timestep) != -1):
+    for i in range(NB_ROBOTS):
+        robot_node[i][0] = getCoordinates(trans_field[i])
+        robot_node[i][1] = getRotation(rota_field[i])
+
+    for i in range(NB_ROBOTS):
+        for j in range(NB_ROBOTS):
+            # register rotation angle of robots in range
+            if ((getDist(*robot_node[i][0], *robot_node[j][0]) <= FOV) and i != j):
+                inBoid[i] = 1
+                orientationMatrix[i][j] = robot_node[j][1]
+                distMatrix[i][j] = robot_node[j][0]
+            else:
+                orientationMatrix[i][j] = 999
+                distMatrix[i][j] = 999
+
+#try to only send relevant info only when necessary otherwise backlog in the robot
+    if counter % 8 == 0:
+        for i in range(NB_ROBOTS):
+            r = "t" + rotationDiff(transform(robot_node[i][1]), transform(avgRotation(orientationMatrix[i])))
+            b = "b" + str(inBoid[i])
+            centermass=centerMass(distMatrix[i])
+            c = "c"+ centerMassdiff(centermass[0],centermass[1],*robot_node[i][0])
+            packet = [r, b,c]
+            emitter.setChannel(i + 1)
+            for item in packet:
+                msg = bytes(item, 'utf-8')
+                emitter.send(msg)
+            counter += 1
+            inBoid[i] = 0;
+    else:
+        counter += 1
+    if counter > 1000:
+        counter = 0
--- boidController/boidController.c
+++ boidController/boidController.c
@@ -13,12 +13,15 @@
 #define MAX_PROX 200
 #define PI acos(-1.0)
 
+#define weight_rotation 5
+#define weight_center 1
+
 char rcv[MSG_LENGTH];
 char cur[15];
 char rota[15];
-char x_str[15];
-char y_str[15];
-double rotation, current, x, y;
+char centr[15];
+double rotation, current, x, y, masscentre;
+
 WbDeviceTag emitter,receiver;
 
 double error = 0;
@@ -40,6 +43,11 @@ void robot_loop() {
 
   wb_receiver_set_channel(receiver, id + 1);
 
+  double change_rota_left=0;
+  double change_rota_right=0;
+  double change_cntr_left=0;
+  double change_cntr_right=0;
+
 
   while (robot_go_on()) {
     receive_msg(rcv);
@@ -59,17 +67,18 @@ void robot_loop() {
       current = strtof(cur, NULL);
       if(current == 1){
         
-      printf("%d is in group\n", id);
+      //printf("%d is in group\n", id);
       }
       //printf("id = %d, c =%f\n", id, current);*/
-    } else if (token == 'x') {
-      strncpy(x_str, rcv + 1, strlen(rcv) - 1);
-      x = strtof(x_str, NULL);
-      //printf("id = %d x = %f\n", id, x);
-    } else if (token == 'y') {
-      strncpy(y_str, rcv + 1, strlen(rcv) - 1);
-      y = strtof(y_str, NULL);
-      //printf("d = %d y = %f\n", id, y);
+    }
+    else if (token == 'c') {
+        strncpy(centr, rcv + 1, strlen(rcv) - 1);
+        masscentre= strtof(centr, NULL);
+        if(masscentre < 0.1 && masscentre > -0.1){
+            masscentre= 0;
+        }
+        //printf("%f the current rotation center of mass\n", masscentre);
+        //rotation = (current + rotation) / 2;
     }
     
     //printf("id = %d r = %f c= %f\n", id, rotation, current);
@@ -77,8 +86,8 @@ void robot_loop() {
     get_prox(prox_values);
 
     // basic explorer behavior
-    double prox_right = (2*prox_values[0] + 2*prox_values[1] + 2*prox_values[2] + 1*prox_values[3]) / 5.;
-    double prox_left = (2*prox_values[7] + 2*prox_values[6] + 2*prox_values[5] + 1*prox_values[4]) / 5.;
+    double prox_right = (2*prox_values[0] + 2*prox_values[1] + 2*prox_values[2] + 1*prox_values[3]) / 7.;
+    double prox_left = (2*prox_values[7] + 2*prox_values[6] + 2*prox_values[5] + 1*prox_values[4]) / 7.;
     double ds_right = (NORM_SPEED * prox_right) / MAX_PROX;
     double ds_left = (NORM_SPEED * prox_left) / MAX_PROX;
     double speed_right = bounded_speed(NORM_SPEED - ds_right);
@@ -88,12 +97,29 @@ void robot_loop() {
     if(prox_right < MAX_PROX && prox_left < MAX_PROX){
       //printf("id = %d, r = %f\n", id, rotation);
       if(rotation > 0){
-        speed_left = bounded_speed(speed_left * 1.5);
-        speed_right = speed_right*0.6;
+          change_rota_left=bounded_speed(speed_left*1.5);
+          change_rota_right=bounded_speed(speed_right*0.6);
+
+        //speed_left = bounded_speed(speed_left * 1.5);
+        //speed_right = speed_right*0.6;
       } else if (rotation < 0){
-        speed_right = bounded_speed(speed_right * 1.5);
-        speed_left = speed_left*0.6;
+          change_rota_left=bounded_speed(speed_left*0.6);
+          change_rota_right=bounded_speed(speed_right*1.5);
+          
+        //speed_right = bounded_speed(speed_right * 1.5);
+        //speed_left = speed_left*0.6;
       }
+      if(masscentre >0){
+          change_cntr_left=bounded_speed(speed_left*1.5);
+          change_cntr_right=bounded_speed(speed_right*0.6);
+      }else if(masscentre <0){
+          change_cntr_left=bounded_speed(speed_left*0.6);
+          change_cntr_right=bounded_speed(speed_right*1.5);
+      }
+      speed_right=bounded_speed((weight_rotation*change_rota_right+weight_center*change_cntr_right)/(weight_rotation+weight_center));
+      speed_left=bounded_speed((weight_rotation*change_rota_left+weight_center*change_cntr_left)/(weight_rotation+weight_center));
+
+
     } else{
       speed_right = bounded_speed(NORM_SPEED - ds_right);
       speed_left = bounded_speed(NORM_SPEED - ds_left);
