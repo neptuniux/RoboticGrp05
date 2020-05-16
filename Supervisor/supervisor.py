e1b0527dd336a167ed0a4819eab72cbd35807bdc
Flavien <flavien.buron@gmail.com>
2020-05-16 15:11:37 +0200

    commented and clean code

--- Supervisor/supervisor.py
+++ Supervisor/supervisor.py
@@ -2,42 +2,45 @@
 
 from controller import *
 import math
+import random
 
 PI = math.pi
 
+### Set to the exact number of robots used
 NB_ROBOTS = 9
+### Controls the "Field of View" of the robots
 FOV = 0.2
-robot = Supervisor()
 
+robot = Supervisor()
 timestep = int(robot.getBasicTimeStep())
 
-coord = (0, 0)
-
+### Initialization of the different variables / matrices used to store the information
 robot_node = [[0]*2 for i in range(NB_ROBOTS)]
 orientationMatrix = [[0]*NB_ROBOTS for i in range(NB_ROBOTS)]
 distMatrix = [[0]*NB_ROBOTS for i in range(NB_ROBOTS)]
 trans_field = []
 rota_field = []
+coord = (0, 0)
 emitter = robot.getEmitter("emitter")
 
-### Initialization. Getting each robots and their postion and rotation field
-for i in range(NB_ROBOTS):
-    node = robot.getFromDef("EP"+str(i))
-    tf = node.getField("translation")
-    rf = node.getField("rotation")
-    trans_field.append(tf)
-    rota_field.append(rf)
+### Set the rotation to a random value in range (-PI/2, PI/2) at launch
+def setRotation(rota_field):
+    values = rota_field.getSFRotation()
+    orientation = random.uniform(-PI/2, PI/2)
+    set = [values[0], values[1], values[2], orientation]
+    rota_field.setSFRotation(set)
+    return
 
 ### Get the coordinates from the translation field
 def getCoordinates(trans_field):
     values = trans_field.getSFVec3f()
-    coord = (values[0], values[2])
+    coord = (values[0], values[2])              # get the relevant values for the X and Z axis, store them in a tuple
     return coord
 
 ### Get rotation from the rotation field
 def getRotation(rota_field):
     values = rota_field.getSFRotation()
-    rota = values[3]
+    rota = values[3]                            # get the relevant values for the rotation around the Z axis
     return rota
 
 ### Calulculate the average rotation from a list of rotation
@@ -67,7 +70,7 @@ def centerMass(*distList):
                 total += 1
     if total == 0 :
         total = 1
-    center = (f'{(x/total):.5f}', f'{(y/total):.5f}')
+    center = ((x/total), (y/total))
     return center
 
 ### Calculate the distance from point(x1,y1) to point(x2,y2)
@@ -81,12 +84,17 @@ def transform(rotation):
         rotation = 2*PI + rotation
     return rotation
 
+### Calculate the angle between the robot and the center of the detected group
+def centerMassdiff(center_x,center_y,robot_x, robot_y):
+    delta = (math.atan2((robot_y - center_y),(robot_x-center_x)))
+    return f'{delta:.5f}'
+
 ### Calculate the difference in rotation between the target rotation and the current one
 def rotationDiff(current, target):
     if target == 0:
         return f'{target:.5f}'
     delta = target - current
-    #get the shortest rotation to reach the target
+    #get the shortest rotation to reach the target (if > PI go the other way around)
     if abs(delta) > PI:
         if current > target:
             delta = (2*PI + target) - current
@@ -94,34 +102,52 @@ def rotationDiff(current, target):
             delta = target - (2*PI + current)
     return f'{delta:.5f}'
 
+
+### Initialization. Getting a handle of each robots and their translation and rotation field
+### Only done once, after launching
+for i in range(NB_ROBOTS):
+    node = robot.getFromDef("EP"+str(i))        # get the handle of a robot from their DEF name
+    tf = node.getField("translation")           # get the handle on the translation field
+    rf = node.getField("rotation")              # get the handle on the translation field
+    setRotation(rf)                             # set the rotation to a random value
+    trans_field.append(tf)                      # Add the access to the translation field to the appropriate array
+    rota_field.append(rf)                       # Add the access to the translation field to the appropriate array
+
 ### Counter value for the execution
 counter = 0
 
 while (robot.step(timestep) != -1):
-    if counter % 5 == 0:
+    if counter % 5 == 0:                        # Only do the calculation and send the info once in a while. For performance and stop backlog at robots
+        # get the coordinates and rotation of each robot
         for i in range(NB_ROBOTS):
             robot_node[i][0] = getCoordinates(trans_field[i])
             robot_node[i][1] = getRotation(rota_field[i])
-
+        # compare each robots to the others
         for i in range(NB_ROBOTS):
             for j in range(NB_ROBOTS):
-                # register rotation angle of robots in range and is not itself
+                # register rotation angle of robots in FOV and is not itself
                 if ((getDist(*robot_node[i][0], *robot_node[j][0]) <= FOV) and i != j):
+                    #matrices for orientation and coordinates of robots in range of robot i
                     orientationMatrix[i][j] = robot_node[j][1]
                     distMatrix[i][j] = robot_node[j][0]
                 else:
+                    # if the robot j is not in range if robot i, put a specific value that can be discarded later
                     orientationMatrix[i][j] = 999
                     distMatrix[i][j] = 999
         ### send information to each robots
         for i in range(NB_ROBOTS):
+            # Calculate the average rotation and the angle to the center of mass of the groupe
             r = "t" + rotationDiff(transform(robot_node[i][1]), transform(avgRotation(orientationMatrix[i])))
-            packet = [r]
+            c = "c" + centerMassdiff(*centerMass(distMatrix[i]),*robot_node[i][0])
+            packet = [r,c]
+            # set emitter channel to the robot i channel
             emitter.setChannel(i + 1)
+            # encryte each information to send, to be read by the receiver
             for item in packet:
                 msg = bytes(item, 'utf-8')
                 emitter.send(msg)
-                counter += 1
+        counter += 1
     else:
         counter += 1
-        if counter > 1000:
-            counter = 0
+    if counter > 1000:
+        counter = 0
--- boidController/boidController.c
+++ boidController/boidController.c
@@ -9,113 +9,96 @@
 #include "../API/epuck/epuckAPI.h"
 #endif
 
-#define MAX_ROTA 2
-#define MAX_PROX 200
-#define PI acos(-1.0)
-
-#define weight_rotation 5
-#define weight_center 1
+/// Weight of the rotation and center of mass adjustment
+#define K_ROTATION 3.0
+#define K_CENTER 2.0
 
 char rcv[MSG_LENGTH];
-char cur[15];
+// strings used to temporarly hold the values received before float transformation
 char rota[15];
 char centr[15];
-double rotation, current, x, y, masscentre;
 
-WbDeviceTag emitter,receiver;
-
-double error = 0;
-double deriv = 0;
-double integ = 0;
-
+double rotation, mass_center;
 
 void robot_setup() {
     init_robot();
     init_prox();
-    //init_communication();
     init_boids();
 }
 
 void robot_loop() {
   int id = get_robot_ID();
   short int prox_values[8];
-  rotation = 0;
+  double rotation = 0, mass_center = 0;
+  double change_rota_left = 0, change_rota_right = 0, change_cntr_left = 0, change_cntr_right = 0;
 
-  wb_receiver_set_channel(receiver, id + 1);
-
-  double change_rota_left=0;
-  double change_rota_right=0;
-  double change_cntr_left=0;
-  double change_cntr_right=0;
+/// Set the channel of the robot to the ID + 1 (see webotsAPI.c)
+  setChannel(id);
 
 
   while (robot_go_on()) {
     receive_msg(rcv);
     char token = rcv[0];
 
+/// Manage received message depending on the token (first char)
     if(token == 't'){
+      // copy the string after the token and convert it to float
       strncpy(rota, rcv + 1, strlen(rcv) - 1);
       rotation = strtof(rota, NULL);
+      // restrict the values to reduce impact small changes
+      //(since the speed adjustment is not proportinal to the value of rotation)
       if(rotation < 0.1 && rotation > -0.1){
         rotation = 0;
       }
     } else if (token == 'c') {
-      strncpy(cur, rcv + 1, strlen(rcv) - 1);
-      current = strtof(cur, NULL);
-    } else if (token == 'c') {
-        strncpy(centr, rcv + 1, strlen(rcv) - 1);
-        masscentre= strtof(centr, NULL);
-        if(masscentre < 0.1 && masscentre > -0.1){
-            masscentre= 0;
+      // copy the string after the token and convert it to float
+      strncpy(centr, rcv + 1, strlen(rcv) - 1);
+        mass_center = strtof(centr, NULL);
+        // restrict the values to reduce impact small changes
+        //(since the speed adjustment is not proportinal to the value of rotation)
+        if(mass_center < 0.1 && mass_center > -0.1){
+            mass_center = 0;
         }
     }
     get_prox(prox_values);
 
     // basic explorer behavior
-    double prox_right = (2*prox_values[0] + 2*prox_values[1] + 2*prox_values[2] + 1*prox_values[3]) / 7.;
-    double prox_left = (2*prox_values[7] + 2*prox_values[6] + 2*prox_values[5] + 1*prox_values[4]) / 7.;
+    double prox_right = (2*prox_values[0] + 2*prox_values[1] + 2*prox_values[2] + 1*prox_values[3]) / 5.;
+    double prox_left = (2*prox_values[7] + 2*prox_values[6] + 2*prox_values[5] + 1*prox_values[4]) / 5.;
     double ds_right = (NORM_SPEED * prox_right) / MAX_PROX;
     double ds_left = (NORM_SPEED * prox_left) / MAX_PROX;
     double speed_right = bounded_speed(NORM_SPEED - ds_right);
     double speed_left = bounded_speed(NORM_SPEED - ds_left);
-    // make sure speed values are legal
 
-    if(prox_right < MAX_PROX && prox_left < MAX_PROX){
+    /// Adjust the speed only if the robot is not currently avoiding an obstacle
+    /// And if there is adjustment to be done
+    if(prox_right < MAX_PROX && prox_left < MAX_PROX && rotation != 0 && mass_center != 0){
       if(rotation > 0){
-          change_rota_left=bounded_speed(speed_left*1.5);
-          change_rota_right=bounded_speed(speed_right*0.6);
-
-        //speed_left = bounded_speed(speed_left * 1.5);
-        //speed_right = speed_right*0.6;
+        change_rota_left = bounded_speed(speed_left*1.5);
+        change_rota_right = bounded_speed(speed_right*0.6);
       } else if (rotation < 0){
-          change_rota_left=bounded_speed(speed_left*0.6);
-          change_rota_right=bounded_speed(speed_right*1.5);
-
-        //speed_right = bounded_speed(speed_right * 1.5);
-        //speed_left = speed_left*0.6;
+        change_rota_left = bounded_speed(speed_left*0.6);
+        change_rota_right = bounded_speed(speed_right*1.5);
       }
-      if(masscentre >0){
-          change_cntr_left=bounded_speed(speed_left*1.5);
-          change_cntr_right=bounded_speed(speed_right*0.6);
-      }else if(masscentre <0){
-          change_cntr_left=bounded_speed(speed_left*0.6);
-          change_cntr_right=bounded_speed(speed_right*1.5);
+      if(mass_center > 0){
+          change_cntr_left = bounded_speed(speed_left*1.5);
+          change_cntr_right = bounded_speed(speed_right*0.6);
+      }else if(mass_center < 0){
+          change_cntr_left = bounded_speed(speed_left*0.6);
+          change_cntr_right = bounded_speed(speed_right*1.5);
       }
-      speed_right=bounded_speed((weight_rotation*change_rota_right+weight_center*change_cntr_right)/(weight_rotation+weight_center));
-      speed_left=bounded_speed((weight_rotation*change_rota_left+weight_center*change_cntr_left)/(weight_rotation+weight_center));
-
-
-    } else{
-      speed_right = bounded_speed(NORM_SPEED - ds_right);
-      speed_left = bounded_speed(NORM_SPEED - ds_left);
+      // Set the speed as a compound of the adjustment from the rotation and center of mass
+      speed_right = bounded_speed((K_ROTATION * change_rota_right + K_CENTER * change_cntr_right)/(K_ROTATION + K_CENTER));
+      speed_left = bounded_speed((K_ROTATION * change_rota_left + K_CENTER * change_cntr_left)/(K_ROTATION + K_CENTER));
     }
+    /// To free a robot that is stuck with its back against a wall
     if(prox_values[3] > 600 && prox_values[4] > 600){
       speed_right = bounded_speed(NORM_SPEED);
       speed_left = bounded_speed(NORM_SPEED);
     }
 
     set_speed(speed_right,speed_left);
-    
+
   }
   cleanup_robot();
 }
